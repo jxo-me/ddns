c9dafed5f028bc4a8473f6334aa11bea277f8722
Monkey <mr.monkey.job@gmail.com>
2023-08-11 18:42:31 +0800

    update webhook

--- config/webhook.go
+++ config/webhook.go
@@ -2,7 +2,15 @@ package config
 
 // Webhook Webhook
 type Webhook struct {
-	WebhookURL         string `json:"webhookURL"`
+	// 支持的变量 #{ipv4Addr}=新的IPv4地址,
+	// #{ipv4Result}=IPv4地址更新结果: 未改变 失败 成功,
+	// #{ipv4Domains}=IPv4的域名，多个以,分割,
+	// #{ipv6Addr}=新的IPv6地址,
+	// #{ipv6Result}=IPv6地址更新结果: 未改变 失败 成功,
+	// #{ipv6Domains}=IPv6的域名，多个以,分割
+	WebhookURL string `json:"webhookURL"`
+	// 如 RequestBody 为空则为 GET 请求，否则为 POST 请求。支持的变量同上
 	WebhookRequestBody string `json:"webhookRequestBody"`
-	WebhookHeaders     string `json:"webhookHeaders"`
+	// 一行一个Header, 如：Authorization: Bearer API_KEY
+	WebhookHeaders string `json:"webhookHeaders"`
 }
